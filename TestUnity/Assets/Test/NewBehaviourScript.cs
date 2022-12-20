4839c6a87176075a77b8a1447ea0fa3fc9675322
MK413g <91109132+MK413g@users.noreply.github.com>
2022-12-20 23:33:34 +0900

    test
    ああああああ

--- TestUnity/Assets/Test/NewBehaviourScript.cs
+++ TestUnity/Assets/Test/NewBehaviourScript.cs
@@ -7,7 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("AAA");   
+        Debug.Log("AAA");
+        Debug.Log("áRŒN‚¿‚á‚ñ‚Æ§ì‚µ‚Ä‚éH");
     }
 
     // Update is called once per frame
